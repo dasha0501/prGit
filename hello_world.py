04a94785e9cd0a2416e9e9be1ec7b0ccf92edafe
dasha0501 <ldvdasha@icoud.com>
2024-10-20 02:43:13 +0900

    Добавлены комментарии в исходный код

--- hello_world.py
+++ hello_world.py
@@ -1,11 +1,13 @@
+"""Функция вывода сообщения"""
 def print_message(message):
-    name = input("Введите ваше имя: ")
-    """Выводит переданное сообщение на экран."""
-    print(message + name)
+    name = input("Введите ваше имя: ") #Инпут для ввода имени
+    print(message + name) #Выводит переданное сообщение на экран
 
+"""Главная функция программы"""
 def main():
     msg = "Hello world from "
-    print_message(msg)
+    print_message(msg) #Вывод сообщения
 
+"""Запуск главной функции"""
 if __name__ == "__main__":
     main()
